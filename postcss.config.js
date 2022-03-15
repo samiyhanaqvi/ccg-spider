1b3982a69a8a75cfe988f339c32ad8ac9f705bee
Chris Arderne <chris@rdrn.me>
2022-03-15 10:07:28 +0000

    Use Tailwind

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: [
+        "default",
+        {
+          normalizeWhitespace: false,
+        },
+      ],
+    }),
+  ],
+};
