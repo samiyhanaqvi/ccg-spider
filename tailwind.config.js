7a140644e0856cd9bff5fd9fa6c3aa3f2e123512
Chris Arderne <chris@rdrn.me>
2022-03-16 17:54:48 +0000

    Mobile styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
+    screens: {
+      sm: { max: "639px" },
+    },
     extend: {
       fontFamily: {
         'sans': ["Titillium Web", "sans-serif"],
