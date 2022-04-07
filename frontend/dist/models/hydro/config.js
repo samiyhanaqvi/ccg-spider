762f9a16ea95d790806165fad7894aff47c1551e
Chris Arderne <chris@rdrn.me>
2022-04-07 12:06:10 +0100

    Update hydro model
    Co-authored-by: leandermue <mueller_leander@outlook.de>

--- frontend/dist/models/hydro/config.js
+++ frontend/dist/models/hydro/config.js
@@ -5,40 +5,17 @@ const loc = {center, zoom};
 const hexSize = 9; // km
 
 const popup = [
+
   {
-    col: "adm1",
-    label: "adm1",
-    unit: "",
-    fmt: false,
-  },
-  {
-    col: "grid_dist",
-    label: "Grid dist",
-    unit: "km",
+    col: "pv",
+    label: "PV",
+    unit: "kWh/kWp per day",
     fmt: true,
   },
   {
-    col: "road_dist",
-    label: "Road dist",
-    unit: "km",
-    fmt: true,
-  },
-  {
-    col: "profit",
-    label: "Profit",
-    unit: "USD/year",
-    fmt: true,
-  },
-  {
-    col: "gov_costs",
-    label: "Gov costs",
-    unit: "USD/year",
-    fmt: true,
-  },
-  {
-    col: "social",
-    label: "Social",
-    unit: "USD/year",
+    col: "price_elec",
+    label: "Electricity price",
+    unit: "€/MWh",
     fmt: true,
   },
 ];
@@ -60,113 +37,32 @@ const infra = [
 
 const pars = [
   {
-    col: "duration",
-    label: "Duration",
-    min: 5,
-    max: 20,
-    val: 10,
-    unit: "years",
-  },
-  {
-    col: "interest_rate",
-    label: "Interest rate",
-    min: 0,
-    max: 100,
-    val: 6,
-    unit: "%",
-  },
-  {
-    col: "labor_per_hh",
-    label: "Labour available",
-    min: 0,
-    max: 10,
-    val: 0.5,
-    unit: "people per hh",
-  },
-  {
-    col: "min_pop",
-    label: "Min pop",
-    min: 0,
-    max: 100000,
-    val: 0,
-    unit: "people",
-  },
-  {
-    col: "max_pop",
-    label: "Max pop",
-    min: 0,
-    max: 1000000,
-    val: 200000,
-    unit: "people",
+    col: "pv_capex",
+    label: "PV CAPEX",
+    min: 500,
+    max: 1000,
+    val: 800,
+    unit: "€/kWp",
   },
 ];
 
 const attrs = [
+
   {
-    col: "tech",
-    label: "technology type",
-    cats: ["solar", "wind"],
-    colors: [
-      "hsla(0, 60%, 60%, 1)",
-      "hsla(100, 60%, 60%, 1)",
-      "hsla(200, 60%, 60%, 0.2)",
-    ],
-  },
-  {
-    col: "road_dist",
-    label: "road distance",
-    min: 0,
-    max: 100,
-    minCol: "hsl(60, 29%, 93%)",
-    maxCol: "hsl(60, 100%, 23%)",
-  },
-  {
-    col: "grid_dist",
-    label: "grid distance",
-    min: 0,
-    max: 100,
+    col: "price_elec",
+    label: "Electricity price",
+    min: 20,
+    max: 25,
     minCol: "hsl(90, 29%, 93%)",
     maxCol: "hsl(90, 100%, 23%)",
   },
   {
-    col: "revenue",
-    label: "revenue",
-    min: 0,
-    max: 55_000_000,
-    minCol: "hsl(30, 29%, 93%)",
-    maxCol: "hsl(30, 100%, 23%)",
-  },
-  {
-    col: "profit",
-    label: "profit",
-    min: 0,
-    max: 15_000_000,
-    minCol: "hsl(0, 29%, 93%)",
-    maxCol: "hsl(0, 100%, 23%)",
-  },
-  {
-    col: "gov_costs",
-    label: "gov costs",
-    min: 0,
-    max: 150_000_000,
-    minCol: "hsl(300, 29%, 93%)",
-    maxCol: "hsl(300, 100%, 23%)",
-  },
-  {
-    col: "gov_annual",
-    label: "gov costs (annual)",
-    min: 0,
-    max: 1,
-    minCol: "hsl(270, 29%, 93%)",
-    maxCol: "hsl(270, 100%, 23%)",
-  },
-  {
-    col: "social",
-    label: "social benefits",
-    min: 0,
-    max: 10,
-    minCol: "hsl(320, 29%, 93%)",
-    maxCol: "hsl(320, 100%, 23%)",
+    col: "pv_radiation",
+    label: "Solar radiation",
+    min: 2,
+    max: 6,
+    minCol: "hsl(255, 29%, 93%)",
+    maxCol: "hsl(255, 100%, 23%)",
   },
 ];
 
--- frontend/dist/models/hydro/model.js
+++ frontend/dist/models/hydro/model.js
@@ -1,28 +1,10 @@
-const getTech = (profit) => {
-  if (profit > 30) {
-    return "solar";
-  } else if (profit > 10) {
-    return "wind";
-  } else {
-    return "none";
-  }
-};
-
 export default (town, pars) => {
-  const revenue = town.city_dist * 2;
-  const profit = revenue / 10;
-  const gov_costs = 100 * pars.duration;
-  const gov_annual = 20;
-  const social = 15;
+  const price_elec = ((pars.pv_capex/20/town.pv/365) * 1000);
+  const pv_radiation = town.pv
 
-  const tech = getTech(profit);
 
   return {
-    tech: tech,
-    revenue: Math.max(0, revenue),
-    profit: Math.max(0, profit),
-    gov_costs: Math.max(0, gov_costs),
-    gov_annual: Math.max(0, gov_annual),
-    social: Math.max(0, social),
+    price_elec: Math.max(0, price_elec),
+    pv_radiation: Math.max(0, pv_radiation),
   };
 };
