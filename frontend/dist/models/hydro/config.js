edf0d4207d8df2a91953679a3a37667f3b7e50c6
leandermue <103034918+leandermue@users.noreply.github.com>
2022-04-08 17:48:36 +0200

    Hydro/improve 08.04.22 (#3)
    * Added wind speed
    
    * Wind Model
    
    * Restore config_example, remove macos files

--- frontend/dist/models/hydro/config.js
+++ frontend/dist/models/hydro/config.js
@@ -13,11 +13,19 @@ const popup = [
     fmt: true,
   },
   {
-    col: "price_elec",
+    col: "wind",
+    label: "Mean wind speed (150m)",
+    unit: "m/s",
+    fmt: true,
+  },
+  {
+    col: "price_elec_wind",
     label: "Electricity price",
     unit: "€/MWh",
     fmt: true,
   },
+
+
 ];
 
 const infra = [
@@ -44,15 +52,48 @@ const pars = [
     val: 800,
     unit: "€/kWp",
   },
+  {
+    col: "wind_capex",
+    label: "Wind CAPEX",
+    min: 750,
+    max: 1500,
+    val: 1000,
+    unit: "€/kW",
+  },
+  {
+    col: "p_turbine",
+    label: "Power size turbine [kW]",
+    min: 1000,
+    max: 5000,
+    val: 3000,
+    unit: "kW",
+  },
+  {
+    col: "interest_rate",
+    label: "Interest rate",
+    min: 0,
+    max: 100,
+    val: 4,
+    unit: "%",
+  },
+
 ];
 
 const attrs = [
 
   {
-    col: "price_elec",
-    label: "Electricity price",
+    col: "price_elec_pv",
+    label: "PV Electricity price",
     min: 20,
     max: 25,
+    minCol: "hsl(90, 29%, 93%)",
+    maxCol: "hsl(90, 100%, 23%)",
+  },
+  {
+    col: "price_elec_wind",
+    label: "Wind Electricity price",
+    min: 0,
+    max: 50,
     minCol: "hsl(90, 29%, 93%)",
     maxCol: "hsl(90, 100%, 23%)",
   },
@@ -61,6 +102,22 @@ const attrs = [
     label: "Solar radiation",
     min: 2,
     max: 6,
+    minCol: "hsl(255, 29%, 93%)",
+    maxCol: "hsl(255, 100%, 23%)",
+  },
+  {
+    col: "wind_speed",
+    label: "Mean wind speed",
+    min: 1,
+    max: 20,
+    minCol: "hsl(255, 29%, 93%)",
+    maxCol: "hsl(255, 100%, 23%)",
+  },
+  {
+    col: "turbine_output",
+    label: "Turbine output",
+    min: 1,
+    max: 20,
     minCol: "hsl(255, 29%, 93%)",
     maxCol: "hsl(255, 100%, 23%)",
   },
--- frontend/dist/models/hydro/model.js
+++ frontend/dist/models/hydro/model.js
@@ -1,10 +1,33 @@
 export default (town, pars) => {
-  const price_elec = ((pars.pv_capex/20/town.pv/365) * 1000);
+
+  //Fixed parameter declaration
+  const pv_lifetime = 20
+  const wind_lifetime = 20
+  const cp = 0.45                     //Coefficient of performance wind turbine 
+  const den_air = 1.14                      //Air density in kg/m3
+  const d_rot = 100                         //Diameter of rotor in kg/m3
+
+
+  
+  function pvf(interestrate,lifetime) {
+    return (((1+interestrate)**lifetime)-1)/(((1+interestrate)**lifetime)*interestrate)
+  }
+  
+  const price_elec_pv = ((pars.pv_capex/pvf((pars.interest_rate/100),pv_lifetime)/town.pv/365) * 1000);
   const pv_radiation = town.pv
+  
+  const wind_speed = town.wind
+  const turbine_output = 0.5 * cp * den_air * ((d_rot**2)*Math.PI/4) * (town.wind ** 3) * 8760 / 1000 / pars.p_turbine    //yearly power output per turbine
+  const price_elec_wind = ((pars.wind_capex/pvf(pars.interest_rate,wind_lifetime))/turbine_output)
+
+
 
 
   return {
-    price_elec: Math.max(0, price_elec),
+    price_elec_pv: Math.max(0, price_elec_pv),
+    price_elec_wind: Math.max(0, price_elec_wind),
+    turbine_output: Math.max(0, turbine_output),
     pv_radiation: Math.max(0, pv_radiation),
+    wind_speed: Math.max(0, wind_speed)
   };
 };
